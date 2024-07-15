5c3440acb0447d46f575f135e13dea1573ce0986
Nick Rogers <nick.rogers@branch.io>
2024-07-15 12:06:05 -0700

    Fix missing $ in web_only param

--- qrToLink.py
+++ qrToLink.py
@@ -179,7 +179,7 @@ def update_links(filename):
                             "~channel" : "QR",
                             "~feature" : "marketing",
                             "~campaign" : "park_QR",
-                            "web_only" : "true"
+                            "$web_only" : "true"
                             }
                     }
                     create_link (url[0], branch_key_live, branch_secret, link_data)
